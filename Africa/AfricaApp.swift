c84ec6483f864f73bcd534c8d982bd0271fdcd77
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-12-09 19:08:51 -0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by kenjimaeda on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
