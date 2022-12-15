7aa7c265386a559ae7a76c9e1db2d20bc6c0c957
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-12-15 11:56:28 -0300

    feat: finished view motion effect

--- Africa/View/MotionEffectView.swift
+++ Africa/View/MotionEffectView.swift
@@ -13,53 +13,54 @@ struct MotionEffectView: View {
 	@State private var randomCircle = Int.random(in: 12...16)
 	@State private var isAnimation = false
 	
+	func randomSize() -> CGFloat {
+		return CGFloat.random(in: 3...30)
+	}
+	
 	func randomPosition(max: CGFloat) -> CGFloat {
 		return CGFloat.random(in: 0...max)
 	}
 	
-	func randomSize() ->  CGFloat  {
-		return CGFloat.random(in: 10...300)
+	func randomScaleEffect() -> CGFloat {
+		return CGFloat(Double.random(in: 0.1...20))
 	}
 	
-	func randomScale() -> CGFloat {
-		return CGFloat(Double.random(in: 0.1...2.0))
-	}
-	
-	func randomSpeed()  -> Double {
+	func randomSped() -> Double  {
 		return Double.random(in: 0.025...1.0)
 	}
 	
-	func randomDealay() -> Double {
+	func randomDelay() -> Double {
 		return Double.random(in: 0...2)
 	}
 	
 	var body: some View {
 		//com geometry consigo pegar o tamanho do dispositivo
 		GeometryReader { geometry in
+			
 			ForEach(0...randomCircle,id: \.self) { item in
 				ZStack {
 					Circle()
 						.foregroundColor(.gray)
 						.opacity(0.15)
-						.frame(width: randomSize(),height: randomSize())
-						.scaleEffect( isAnimation ? randomScale() : 1)
+						.frame(width: randomSize(),height: randomSize(),alignment: .center)
+						.scaleEffect(isAnimation ? randomScaleEffect() : 1)
 						.position(
-							//vai ocupar maximo do tamanho da tela
 							x: randomPosition(max: geometry.size.width),
 							y: randomPosition(max: geometry.size.height)
 						)
-						.animation(Animation.interpolatingSpring(stiffness: 0.5, damping: 0.5)
-							.speed(randomSpeed())
-							.delay(randomDealay())
-											 , value: isAnimation)
-					
-						.onAppear {
-							isAnimation = true
-						}
-					
-				} // Zstack
-			} // Loop
-		} //Geometry
+						.animation(
+							Animation.interpolatingSpring(stiffness: 0.5, damping: 0.5)
+								.repeatForever()
+								.delay(randomSped())
+								.speed(randomDelay())
+							, value: isAnimation)
+				} //ZStack
+				.onAppear {
+					isAnimation = true
+				}
+			}// Loop
+			
+		}//Geometry
 		//drawingGroup e ideal para performace
 		.drawingGroup()
 	}
